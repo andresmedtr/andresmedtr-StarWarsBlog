269d46e7db4b3aef071bf7312d187fd41be27fd2
Andres Medina <123752360+andresmedtr@users.noreply.github.com>
2024-07-08 23:12:21 +0000

    modified template functions

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,18 +1,6 @@
 const getState = ({ getStore, getActions, setStore }) => {
   return {
     store: {
-      demo: [
-        {
-          title: "FIRST",
-          background: "white",
-          initial: "white",
-        },
-        {
-          title: "SECOND",
-          background: "white",
-          initial: "white",
-        },
-      ],
       people: [],
       planets: [],
       vehicles: [],
@@ -20,10 +8,6 @@ const getState = ({ getStore, getActions, setStore }) => {
     },
     actions: {
       // Use getActions to call a function within a fuction
-      exampleFunction: () => {
-        getActions().changeColor(0, "green");
-      },
-
       addFavorites: (name) => {
         const store = getStore();
         setStore({ favorites: [...store.favorites, name] });
@@ -61,21 +45,6 @@ const getState = ({ getStore, getActions, setStore }) => {
             setStore({ vehicles: data.results });
           })
           .catch((err) => console.error(err));
-      },
-
-      changeColor: (index, color) => {
-        //get the store
-        const store = getStore();
-
-        //we have to loop the entire demo array to look for the respective index
-        //and change its color
-        const demo = store.demo.map((elm, i) => {
-          if (i === index) elm.background = color;
-          return elm;
-        });
-
-        //reset the global store
-        setStore({ demo: demo });
       },
     },
   };
